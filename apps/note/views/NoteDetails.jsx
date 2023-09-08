5400182420d1e1725fd48dc32d36b16e48e0b961
Ofir Kaspi <kaspiofir@gmail.com>
2023-09-08 15:31:16 +0300

    Hover buttons styling

--- apps/note/views/NoteDetails.jsx
+++ apps/note/views/NoteDetails.jsx
@@ -27,35 +27,75 @@ export function NoteDetails() {
       })
   }, [noteId])
 
+  function handleSubmit(ev) {
+    console.log(ev)
+    ev.preventDefault()
+    // onAddNote(noteInfo)
+  }
+
+  function handleChange({ target }) {
+    const field = target.name
+    let value = target.value
+    switch (target.type) {
+      case 'number':
+      case 'range':
+        value = +value || ''
+        break
+      case 'checkbox':
+        value = target.checked
+        break
+      default:
+        break
+    }
+    setNote(prevNote => ({ ...prevNote, info: { ...prevNote.info, [field]: value } }))
+  }
+
   if (!note) return <div className="loading">Loading...</div>
   return (
     <div className="note-details-overlay" onClick={() => onExitModal()}>
-      <div className="note-details" onClick={e => e.stopPropagation()}>
-        <div>
-          {note.info.url && <img src={note.info.url} alt="user-pic" />}
-          <input type="text" value={note.info.title} placeholder="Title" />
-          <input type="text" value={note.info.txt} placeholder="Take a note..." />
-        </div>
-        <div>{note.createdAt}</div>
-        <div>
-          <button>
-            <span className="material-symbols-outlined">delete</span>
-          </button>
-          <button>
-            <span className="material-symbols-outlined">star</span>
-          </button>
-          <button>
-            <span className="material-symbols-outlined">palette</span>
-          </button>
-          <button>
-            <span className="material-symbols-outlined">new_label</span>
-          </button>
-          <button>
-            <span className="material-symbols-outlined">image</span>
-          </button>
-          <button>
-            <span className="material-symbols-outlined">more_vert</span>
-          </button>
+      <div className="note-details flex column" onClick={e => e.stopPropagation()}>
+        {note.info.url && <img src={note.info.url} alt="user-pic" />}
+        <form
+          id="note-details-form"
+          className="note-details-form flex column"
+          onSubmit={handleSubmit}
+        >
+          <input type="text" value={note.info.title} placeholder="Title" onChange={handleChange} />
+          <input
+            type="text"
+            value={note.info.txt}
+            placeholder="Take a note..."
+            onChange={handleChange}
+          />
+        </form>
+        <div className="create-at">{note.createdAt}</div>
+        <div className="flex space-between align-center">
+          <div className="note-details-btns">
+            <button>
+              <span className="material-symbols-outlined">file_copy</span>
+            </button>
+            <button>
+              <span className="material-symbols-outlined">star</span>
+            </button>
+            <button>
+              <span className="material-symbols-outlined">palette</span>
+            </button>
+            <button>
+              <span className="material-symbols-outlined">new_label</span>
+            </button>
+            <button>
+              <span className="material-symbols-outlined">image</span>
+            </button>
+            <button>
+              <span className="material-symbols-outlined">more_vert</span>
+            </button>
+          </div>
+          <div>
+            {/* TODO: the submission should be execute just whenclick enter or clicking ouside of the form div  */}
+            <button className="close-btn" type="submit" form="note-details-form">
+              Close
+            </button>
+          </div>
         </div>
 
         {/* <NoteEdit note={note} /> */}
