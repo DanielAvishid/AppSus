7b8d15957c168f66d95e22db920663395bcdf556
Daniel Avishid <danielavishid@gmail.com>
2023-09-10 04:13:23 +0300

    check something

--- apps/note/cmps/NoteList.jsx
+++ apps/note/cmps/NoteList.jsx
@@ -1,44 +1,44 @@
-import { NotePreview } from './NotePreview.jsx'
-import { NoteAdd } from '../cmps/NoteAdd.jsx'
+// import { NotePreview } from './NotePreview.jsx'
+// import { NoteAdd } from '../cmps/NoteAdd.jsx'
 
-const { useNavigate } = ReactRouterDOM
-const { Outlet, useOutletContext } = ReactRouterDOM
+// const { useNavigate } = ReactRouterDOM
+// const { Outlet, useOutletContext } = ReactRouterDOM
 
-export function NoteList() {
-  const [notes, onRemoveNote, getEmptyNote, onAddNote] = useOutletContext()
-  // const navigate = useNavigate()
-  console.log(notes)
+// export function NoteList() {
+//   const [notes, onRemoveNote, getEmptyNote, onAddNote] = useOutletContext()
+//   // const navigate = useNavigate()
+//   console.log(notes)
 
-  const pinnedNotes = notes.filter(note => note.isPinned === true)
-  const regularNotes = notes.filter(note => note.isPinned === false)
+//   const pinnedNotes = notes.filter(note => note.isPinned === true)
+//   const regularNotes = notes.filter(note => note.isPinned === false)
 
-  return (
-    <section className="note-list-container">
-      <NoteAdd onAddNote={onAddNote} getEmptyNote={getEmptyNote} />
-      <section className="notes-container">
-        <p>PINNED</p>
-        <section className="pinned-container">
-          {pinnedNotes.map(note => (
-            <NotePreview
-              key={note.id}
-              note={note}
-              onAddNote={onAddNote}
-              onRemoveNote={onRemoveNote}
-            />
-          ))}
-        </section>
-        <p>OTHERS</p>
-        <section className="other-container">
-          {regularNotes.map(note => (
-            <NotePreview
-              key={note.id}
-              note={note}
-              onAddNote={onAddNote}
-              onRemoveNote={onRemoveNote}
-            />
-          ))}
-        </section>
-      </section>
-    </section>
-  )
-}
+//   return (
+//     <section className="note-list-container">
+//       <NoteAdd onAddNote={onAddNote} getEmptyNote={getEmptyNote} />
+//       <section className="notes-container">
+//         <p>PINNED</p>
+//         <section className="pinned-container">
+//           {pinnedNotes.map(note => (
+//             <NotePreview
+//               key={note.id}
+//               note={note}
+//               onAddNote={onAddNote}
+//               onRemoveNote={onRemoveNote}
+//             />
+//           ))}
+//         </section>
+//         <p>OTHERS</p>
+//         <section className="other-container">
+//           {regularNotes.map(note => (
+//             <NotePreview
+//               key={note.id}
+//               note={note}
+//               onAddNote={onAddNote}
+//               onRemoveNote={onRemoveNote}
+//             />
+//           ))}
+//         </section>
+//       </section>
+//     </section>
+//   )
+// }
--- apps/note/cmps/NotePallete.jsx
+++ apps/note/cmps/NotePallete.jsx
@@ -1,18 +1,18 @@
-export function NotePallete({ note, onChangeNoteBgc }) {
-  return (
-    <div className="color-pallete flex">
-      <button className="transparent" onClick={() => onChangeNoteBgc('transparent', note)}>
-        ✖
-      </button>
-      <button className="lightblue" onClick={() => onChangeNoteBgc('lightblue', note)}></button>
-      <button className="lightgreen" onClick={() => onChangeNoteBgc('lightgreen', note)}></button>
-      <button className="lightpink" onClick={() => onChangeNoteBgc('lightpink', note)}></button>
-      <button className="lightcoral" onClick={() => onChangeNoteBgc('lightcoral', note)}></button>
-      <button className="lightsalmon" onClick={() => onChangeNoteBgc('lightsalmon', note)}></button>
-      <button
-        className="lightseagreen"
-        onClick={() => onChangeNoteBgc('lightseagreen', note)}
-      ></button>
-    </div>
-  )
-}
+// export function NotePallete({ note, onChangeNoteBgc }) {
+//   return (
+//     <div className="color-pallete flex">
+//       <button className="transparent" onClick={() => onChangeNoteBgc('transparent', note)}>
+//         ✖
+//       </button>
+//       <button className="lightblue" onClick={() => onChangeNoteBgc('lightblue', note)}></button>
+//       <button className="lightgreen" onClick={() => onChangeNoteBgc('lightgreen', note)}></button>
+//       <button className="lightpink" onClick={() => onChangeNoteBgc('lightpink', note)}></button>
+//       <button className="lightcoral" onClick={() => onChangeNoteBgc('lightcoral', note)}></button>
+//       <button className="lightsalmon" onClick={() => onChangeNoteBgc('lightsalmon', note)}></button>
+//       <button
+//         className="lightseagreen"
+//         onClick={() => onChangeNoteBgc('lightseagreen', note)}
+//       ></button>
+//     </div>
+//   )
+// }
