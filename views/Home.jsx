1433ecc84507f53db7931d7748afad82c7c0a9dd
Ofir Kaspi <kaspiofir@gmail.com>
2023-09-10 08:29:17 +0300

    changed navigate

--- views/Home.jsx
+++ views/Home.jsx
@@ -3,25 +3,36 @@ import { AppHeader } from '../cmps/AppHeader.jsx'
 const { useNavigate } = ReactRouterDOM
 
 export function Home() {
-    const navigate = useNavigate()
-    return (
-        <section className="home">
-            <AppHeader />
-            <section className='hero'>
-                <img src="./././img/hero.jpg" alt="" />
-                <div className='hero-content'>
-                    <h1>AppSus</h1>
-                    <div className='service-container flex align-center justify-center'>
-                        <p className='flex align-center'>Mail Services <img onClick={() => navigate('/mail/list')} className='logo logo-service' src="./././img/gmail-logo.png" alt="" /></p>
-                        <p className='flex align-center'>Keep Services
-                            <img onClick={() => navigate('/note')}
-                                className="logo logo-keep"
-                                src='./././img/Google_Keep_logo.png' />
-                        </p>
-                    </div>
-                    <h2>Expanding in future</h2>
-                </div>
-            </section>
-        </section>
-    )
+  const navigate = useNavigate()
+  return (
+    <section className="home">
+      <AppHeader />
+      <section className="hero">
+        <img src="./././img/hero.jpg" alt="" />
+        <div className="hero-content">
+          <h1>AppSus</h1>
+          <div className="service-container flex align-center justify-center">
+            <p className="flex align-center">
+              Mail Services{' '}
+              <img
+                onClick={() => navigate('/mail/list')}
+                className="logo logo-service"
+                src="./././img/gmail-logo.png"
+                alt=""
+              />
+            </p>
+            <p className="flex align-center">
+              Keep Services
+              <img
+                onClick={() => navigate('/note/list')}
+                className="logo logo-keep"
+                src="./././img/Google_Keep_logo.png"
+              />
+            </p>
+          </div>
+          <h2>Expanding in future</h2>
+        </div>
+      </section>
+    </section>
+  )
 }
