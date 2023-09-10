c36701629c4d9619b15c1b30f5b968e03a1074a7
Daniel Avishid <danielavishid@gmail.com>
2023-09-10 04:06:18 +0300

    fix

--- views/Home.jsx
+++ views/Home.jsx
@@ -9,20 +9,17 @@ export function Home() {
             <AppHeader />
             <section className='hero'>
                 <img src="./././img/hero.jpg" alt="" />
-                <div>
-                    <div className='hero-content'>
-                        <h1>AppSus</h1>
-                        <div className='service-container flex align-center justify-center'>
-                            <p className='flex align-center'>Mail Services <img onClick={() => navigate('/mail/list')} className='logo logo-service' src="./././img/gmail-logo.png" alt="" /></p>
-                            <p className='flex align-center'>Keep Services
-                                <img onClick={() => navigate('/note')}
-                                    className="logo logo-keep"
-                                    src='./././img/Google_Keep_logo.png'
-                                />
-                            </p>
-                        </div>
-                        <h2>Expanding in future</h2>
+                <div className='hero-content'>
+                    <h1>AppSus</h1>
+                    <div className='service-container flex align-center justify-center'>
+                        <p className='flex align-center'>Mail Services <img onClick={() => navigate('/mail/list')} className='logo logo-service' src="./././img/gmail-logo.png" alt="" /></p>
+                        <p className='flex align-center'>Keep Services
+                            <img onClick={() => navigate('/note')}
+                                className="logo logo-keep"
+                                src='./././img/Google_Keep_logo.png' />
+                        </p>
                     </div>
+                    <h2>Expanding in future</h2>
                 </div>
             </section>
         </section>
